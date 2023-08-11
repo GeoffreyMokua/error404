27de1e8e8d06b90f9b372d18779108fb8eae4039
Geoffrey Nyakundi <arisimokua280@gmail.com>
2023-08-11 16:17:15 +0300

    error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image404 from './image404';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+      <div className="site-content">
+        <div className="content-wrapper section-pb">
+          <section className="section-ptb bg-light">
+            <Container>
+              <Row className="justify-content-center">
+                <Col md={10} className="text-center">
+                  <div className="error-404">
+                    <Image404 />
+                    <h2 className="title mt-5">Oops! Page Not Found</h2>
+                    <h6>
+                      The page you are looking for might have been removed had
+                      its name changed or is temporarily unavailable.
+                    </h6>
+                    
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+        </div>
+      </div>
+    </>
   );
 }
 
